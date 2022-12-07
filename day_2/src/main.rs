5ef6a7259e52f2c87b1f30d76ae55a9ac88620a6
seagleNet <marc.sommerau@gmail.com>
2022-12-07 21:49:35 +0100

    finish day_2 part 1

--- day_2/src/main.rs
+++ day_2/src/main.rs
@@ -2,6 +2,7 @@ use std::{
     fs::File,
     io::{prelude::*, BufReader},
     path::Path,
+    process::exit,
     vec::Vec,
 };
 
@@ -13,12 +14,59 @@ fn lines_from_file(filename: impl AsRef<Path>) -> Vec<String> {
         .collect()
 }
 
+fn referee(play: String) -> i32 {
+    let picks: Vec<&str> = play.split_whitespace().collect();
+    let elf_pick: &str = picks[0];
+    let player_pick: &str = picks[1];
+    let mut points: i32 = 0;
+
+    if player_pick == "X" {
+        points += 1;
+        match elf_pick {
+            "A" => points += 3, // d
+            "B" => points += 0, // l
+            "C" => points += 6, // w
+            &_ => {
+                eprintln!("Invalid elf pick!");
+                exit(1);
+            }
+        }
+    } else if player_pick == "Y" {
+        points += 2;
+        match elf_pick {
+            "A" => points += 6, // w
+            "B" => points += 3, // d
+            "C" => points += 0, // l
+            &_ => {
+                eprintln!("Invalid elf pick!");
+                exit(1);
+            }
+        }
+    } else if player_pick == "Z" {
+        points += 3;
+        match elf_pick {
+            "A" => points += 0, // l
+            "B" => points += 6, // w
+            "C" => points += 3, // d
+            &_ => {
+                eprintln!("Invalid elf pick!");
+                exit(1);
+            }
+        }
+    } else {
+        eprintln!("Invalid player pick!");
+        exit(1);
+    }
+    return points;
+}
+
 fn main() {
     let strategies = lines_from_file("./day_2/day_2.in");
+    let mut player_score: i32 = 0;
 
-    for (_i, picks) in strategies.iter().enumerate() {
-        println!("{picks}");
-        //let elf_pick: String = picks.split(" ").nth(1).map(String::from);
-        //let player_pick: String = picks.split(" ").nth(2).map(String::from);
+    for play in strategies {
+        player_score += referee(play);
     }
+
+    println!("Player points: {}", player_score)
 }

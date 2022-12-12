934be332a8524b6ee575b8bbc2c6220cf024bcd0
seagleNet <marc.sommerau@gmail.com>
2022-12-13 00:17:26 +0100

    progress day 4

--- day_4/src/main.rs
+++ day_4/src/main.rs
@@ -1,11 +1,15 @@
 use file_utils::lines_from_file;
+use std::str::FromStr;
 
-fn get_pair_ranges(pair: &str) -> Vec<&str> {
-    pair.split(",").collect::<Vec<&str>>()
+fn get_pair_ranges(pair: &str) -> (Vec<i32>, Vec<i32>) {
+    let (left, right) = pair.split_once(',').unwrap();
+    (left.split('-').filter_map(|v| i32::from_str(v).ok()).collect::<Vec<i32>>(), right.split('-').filter_map(|v| i32::from_str(v).ok()).collect::<Vec<i32>>())
 }
 
 fn main() {
     let pairs = lines_from_file("./day_4/day_4.in");
+
+    for pair in pairs {}
 }
 
 #[cfg(test)]
@@ -27,5 +31,13 @@ mod tests {
         let (left, right) = pair.split_once(',').unwrap();
         assert_eq!("2-4", left);
         assert_eq!("6-8", right);
+    }
+
+    #[test]
+    fn test_split_pair_v3() {
+        let pair: &str = "2-4,6-8";
+        let (left, right) = get_pair_ranges(pair);
+        assert_eq!(vec![2,4], left);
+        assert_eq!(vec![6,8], right);
     }
 }

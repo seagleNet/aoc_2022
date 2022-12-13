e2231e02aabb19a3eaed09fbc377890fd2b931cd
seagleNet <marc.sommerau@gmail.com>
2022-12-13 23:15:29 +0100

    optimizations for day 4 pt 1

--- day_4/src/main.rs
+++ day_4/src/main.rs
@@ -1,23 +1,25 @@
 use file_utils::lines_from_file;
-use std::str::FromStr;
 
 fn get_pair_ranges(pair: &str) -> (Vec<i32>, Vec<i32>) {
     let (left, right) = pair.split_once(',').unwrap();
     (
         left.split('-')
-            .filter_map(|v| i32::from_str(v).ok())
+            .map(|v| v.parse::<i32>().unwrap())
             .collect::<Vec<i32>>(),
         right
             .split('-')
-            .filter_map(|v| i32::from_str(v).ok())
+            .map(|v| v.parse::<i32>().unwrap())
             .collect::<Vec<i32>>(),
     )
 }
 
 fn compare_ranges(left: Vec<i32>, right: Vec<i32>) -> i32 {
-    if left.iter().min() >= right.iter().min() && left.iter().max() <= right.iter().max() {
+    let range_l = *left.iter().min().unwrap()..=*left.iter().max().unwrap();
+    let range_r = *right.iter().min().unwrap()..=*right.iter().max().unwrap();
+
+    if range_l.contains(&right[0]) && range_l.contains(&right[1]) {
         return 1;
-    } else if right.iter().min() >= left.iter().min() && right.iter().max() <= left.iter().max() {
+    } else if range_r.contains(&left[0]) && range_r.contains(&left[1]) {
         return 1;
     } else {
         return 0;
@@ -28,17 +30,15 @@ fn main() {
     let pairs = lines_from_file("./day_4/day_4.in");
 
     let result = pairs
-            .iter()
-            .map(|pair| get_pair_ranges(pair))
-            .map(|ranges| compare_ranges(ranges.0, ranges.1))
-            .fold(0, |current, v| current + v);
+        .iter()
+        .map(|pair| get_pair_ranges(pair))
+        .map(|ranges| compare_ranges(ranges.0, ranges.1))
+        .sum::<i32>();
     println!("Total fully contained: {}", result);
 }
 
 #[cfg(test)]
 mod tests {
-    use std::result;
-
     use super::*;
 
     #[test]
@@ -73,7 +73,7 @@ mod tests {
             .iter()
             .map(|pair| get_pair_ranges(pair))
             .map(|ranges| compare_ranges(ranges.0, ranges.1))
-            .fold(0, |current, v| current + v);
+            .sum();
         assert_eq!(1, result);
     }
 }

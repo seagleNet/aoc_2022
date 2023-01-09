0e31e67943535568d17fa32d95278f74dd6a1b14
seagleNet <marc.sommerau@gmail.com>
2023-01-10 00:48:01 +0100

    progress day 11

--- day_11/src/main.rs
+++ day_11/src/main.rs
@@ -1,8 +1,86 @@
 use file_utils::lines_from_file;
+use lazy_static::lazy_static;
+use regex::Regex;
 use std::collections::HashMap;
 
+struct Monkey {
+    id: i32,
+    items: Vec<i32>,
+    operation: (String, String),
+    test_divisible: i32,
+    true_dest: i32,
+    false_dest: i32,
+}
+
+fn parse_input(lines: Vec<String>, monkey_id: &i32) -> Monkey {
+    let mut monkey_title = String::from("Monkey ");
+    let mut input_items = Vec::new();
+    let mut input_operation = ('x'.to_string(), "num".to_string());
+    let mut input_test_divisible = 0;
+    let mut input_true_dest = 0;
+    let mut input_false_dest = 0;
+    lazy_static! {
+        static ref RE: Regex =
+            Regex::new(r"^(?:  )*(?<key>[a-zA-Z0-1 ]+): ?(?<value>.*)$").unwrap();
+    }
+
+    monkey_title.push_str(&monkey_id.to_string());
+
+    for line in lines {
+        if let Some(captures) = RE.captures(line.as_str()) {
+            match &captures["key"] {
+                x if x.contains("Monkey") => (),
+                "Starting items" => {
+                    input_items = captures["value"]
+                        .split(", ")
+                        .map(|f| f.parse::<i32>().unwrap())
+                        .collect();
+                }
+                "Operation" => {
+                    let input: Vec<&str> = captures["value"]
+                        .split_whitespace().collect();
+                    input_operation = (input[3].to_string(), input[4].to_string())
+                }
+                "Test" => {
+                    let input: Vec<&str> = captures["value"].split_whitespace().collect();
+                    input_test_divisible = input[2].parse::<i32>().unwrap();
+                }
+                "If true" => {
+                    let input: Vec<&str> = captures["value"].split_whitespace().collect();
+                    input_true_dest = input[3].parse::<i32>().unwrap();
+                }
+                "If false" => {
+                    let input: Vec<&str> = captures["value"].split_whitespace().collect();
+                    input_false_dest = input[3].parse::<i32>().unwrap();
+                }
+                _ => panic!(),
+            }
+        }
+    }
+
+    Monkey {
+        id: *monkey_id,
+        items: input_items,
+        operation: input_operation,
+        test_divisible: input_test_divisible,
+        true_dest: input_true_dest,
+        false_dest: input_false_dest,
+    }
+}
+
 fn pt1(lines: Vec<String>) -> i32 {
-    return 10605
+    let mut monkey_id = 0;
+    let mut monkey_file: Vec<String> = Vec::new();
+
+    for line in lines {
+        monkey_file.push(line.clone());
+        if line.is_empty() {
+            parse_input(monkey_file, &monkey_id);
+            monkey_file = Vec::new();
+            monkey_id += 1;
+        }
+    }
+    return 10605;
 }
 
 fn main() {
